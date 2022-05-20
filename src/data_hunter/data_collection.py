56c6ec4d0819d2cdd741424208ffa41c92b9ae83
pablorl94 <pablorl94@gmail.com>
2022-05-20 19:28:18 +0200

    Change the naming convention for stories (formerly texts) from data collection module

--- src/data_hunter/data_collection.py
+++ src/data_hunter/data_collection.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 SCRAPED_DATA_PATH = './scraped_data'
 
-TEXTS_URL = 'http://www.cs.toronto.edu/~atef/stories.zip'
+STORIES_URL = 'http://www.cs.toronto.edu/~atef/stories.zip'
 ALIGNMENTS_URL = 'http://www.cs.toronto.edu/~atef/manual_alignments.zip'
 
 
@@ -25,12 +25,12 @@ def setup_environment():
     os.makedirs(SCRAPED_DATA_PATH, exist_ok=True)
 
 
-def get_texts():
-    """Get the texts."""
-    logger.info(f"Collecting texts from '{TEXTS_URL}'.")
-    response = requests.get(TEXTS_URL)
+def get_stories():
+    """Get the stories."""
+    logger.info(f"Collecting stories from '{STORIES_URL}'.")
+    response = requests.get(STORIES_URL)
 
-    logger.info("Decompressing texts files.")
+    logger.info("Decompressing stories files.")
     files = ZipFile(BytesIO(response.content))
     files.extractall(SCRAPED_DATA_PATH)
     logger.info("Decompression finished successfully.")
@@ -44,7 +44,7 @@ def get_summaries():
 
 def get_alignments():
     """Get the manual alignments."""
-    logger.info(f"Collecting texts from '{ALIGNMENTS_URL}'.")
+    logger.info(f"Collecting alignments from '{ALIGNMENTS_URL}'.")
     response = requests.get(ALIGNMENTS_URL)
 
     logger.info("Decompressing alignments files.")
@@ -54,16 +54,16 @@ def get_alignments():
 
 
 def get_all():
-    """Get the texts, summaries and manual alignments."""
-    get_texts()
+    """Get the stories, summaries and manual alignments."""
+    get_stories()
     get_summaries()
     get_alignments()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Data hunter process parser')
-    parser.add_argument('-r', '--resource', choices=['texts', 'summaries', 'alignments', 'all'], default='all',
-                        help="Resource to collect ['texts', 'summaries', 'alignments', 'all']. Defaults 'all'.")
+    parser.add_argument('-r', '--resource', choices=['stories', 'summaries', 'alignments', 'all'], default='all',
+                        help="Resource to collect ['stories', 'summaries', 'alignments', 'all']. Defaults 'all'.")
     args = parser.parse_args()
 
     setup_environment()
